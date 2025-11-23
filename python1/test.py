35daf2af1cc49da9e5af0c02f0ae80e24b3a8b59
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2025-11-23 19:37:59 +0700

    all file for teach

---
+++ python1/test.py
@@ -0,0 +1,14 @@
+# data = [(1, 2), (3, 4), (5, 6)]
+# tuple_of_lists = tuple(map(list, zip(*data)))
+# print(tuple_of_lists)
+# Output: ([1, 3, 5], [2, 4, 6])
+
+# students = [("Alice", 18, 85), ("Bob", 18, 90), ("Charlie", 19, 78)]
+# students.sort(key=lambda x: x[2], reverse=True)
+
+# print(students)
+
+def add(a, b):
+    return a + b
+
+add(3, 5)
