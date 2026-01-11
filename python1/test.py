8486d4e27cb5f707c3608893894a2637db9b7397
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-01-11 19:48:31 +0700

    day11 to day14

--- python1/test.py
+++ python1/test.py
@@ -1,14 +1,22 @@
-# data = [(1, 2), (3, 4), (5, 6)]
-# tuple_of_lists = tuple(map(list, zip(*data)))
-# print(tuple_of_lists)
-# Output: ([1, 3, 5], [2, 4, 6])
-
-# students = [("Alice", 18, 85), ("Bob", 18, 90), ("Charlie", 19, 78)]
-# students.sort(key=lambda x: x[2], reverse=True)
-
-# print(students)
-
-def add(a, b):
-    return a + b
-
-add(3, 5)
+import random
+import re
+nums = [random.randint(1,9) for i in range(4)]
+print(nums)
+equation = input()
+used_nums = list(map(int, re.findall(r'\d+', equation)))
+temp = nums.copy()
+valid = True
+for num in used_nums:
+    if num in temp:
+        temp.remove(num)
+    else:
+        valid = False
+        break
+if not valid:
+    print("wrong input")
+else:
+    result = eval(equation)
+    if result == 24:
+        print("correct")
+    else:
+        print("incorrect")
