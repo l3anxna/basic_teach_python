7672fd1c98304fbfa69388c2973ec7a7f857f8be
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-03-01 19:56:35 +0700

    day 20

--- python1/test.py
+++ python1/test.py
@@ -1,22 +1,18 @@
-import random
-import re
-nums = [random.randint(1,9) for i in range(4)]
-print(nums)
-equation = input()
-used_nums = list(map(int, re.findall(r'\d+', equation)))
-temp = nums.copy()
-valid = True
-for num in used_nums:
-    if num in temp:
-        temp.remove(num)
-    else:
-        valid = False
-        break
-if not valid:
-    print("wrong input")
-else:
-    result = eval(equation)
-    if result == 24:
-        print("correct")
-    else:
-        print("incorrect")
+import  numpy as np
+from numpy.ma.core import dot
+
+arr = np.array([[1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10],
+                [11, 12, 13, 14, 15],
+                [16, 17, 18, 19, 20]])
+print(arr.mean())
+print(arr.sum())
+print(arr.max())
+print(arr.min())
+matrix1 = np.array([[6, 7],
+                    [11, 5]])
+matrix2 = np.array([[11, 10],
+                    [10, 5]])
+print(matrix1+matrix2)
+print(matrix1*matrix2)
+print(np.dot(matrix1,matrix2))
