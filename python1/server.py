f056992fa4412feeed1282c16da6450fff9ac9fe
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-08-03 19:34:04 +0700

    multi-user server

--- python1/server.py
+++ python1/server.py
@@ -1,20 +1,34 @@
 import socket
+import threading
 
-server = socket.socket() #Create a TCP socket object
+server = socket.socket()
+
 server.bind(("localhost", 6767))
-
 server.listen()
+
 print("Server is listening on port 6767...")
-client, addr = server.accept() #Accept a connection from a client
-#.accept() is a blocking function, it pause execution until a client connects to the server
-print(f"Connection from {addr} has been established!")
+
+connections = 0
+def handle_client(client):
+    while True:
+        msg = client.recv(1024)
+
+        if not msg:
+            break
+
+        print(f"Received message: {msg.decode()}") #Print the received message
+        print(threading.current_thread().name) #Print the name of the current thread
+
+        client.send(msg) #Echo the message back to the client
+
+    client.close()
+
+lock = threading.Lock()
 
 while True:
-    message = client.recv(1024) #Receive a message from the client
-
-    if not message:
-        break
-
-    client.send(message) #Echo the message back to the client
-
-client.close() #Close the connection with the client
+    client, addr = server.accept()
+    with lock:
+        connections += 1
+    
+    thread = threading.Thread(target=handle_client, args=(client,))
+    thread.start()
